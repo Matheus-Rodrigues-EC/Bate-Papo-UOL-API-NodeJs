a74b1a2a6e6c035a57a1bea76129fb61f92c90ce
Matt Rodrigues <ec.mattrodrigues@gmail.com>
2023-04-16 19:16:50 -0300

    Refactor methodos for use Async/await and try/catch

--- src/app.js
+++ src/app.js
@@ -46,62 +46,74 @@ let db;
 let participantsList;
 
 mongoClient.connect()
-    .then(() => console.log("DataBase Connected..."))
+    .then(() => {
+        console.log("DataBase Connected...")
+        db = mongoClient.db()
+    })
     .catch((error) => console.log(error.message));
 //------------------------------------------------------------------
 
 
 // GET Participantes --------------------------------------------
 
-app.get("/participants", (req, res) => {
-    db = mongoClient.db()
-    db.collection("participants").find().toArray()
-        .then((participants) => {
-            participantsList = participants;
+app.get("/participants", async (req, res) => {
+    try{
+        const participants = await db.collection("participants").find().toArray()
+        if(participants){
+            participantsList = participants;    
             res.send(participants);
-            // console.log(participantsList);
-        })
-        .catch((error) => {
-            res.status(500).send(error.message);
-            return;
-        });
+        }
+    }catch(error){
+        return res.status(500).send(error.message);
+    }
 })
 
 // Fim GET Participantes ----------------------------------------
 
 
 // POST participante ----------------------------------------------
-app.post("/participants", (req, res) => {
+app.post("/participants", async (req, res) => {
     const {name} = req.body;
     const time = dayjs().format('HH:mm:ss');
 
-    if(nameSchema.validate({username: name}).error === undefined){
-// Faz a validação do nome de usuário
-        if(participantsList.find((user) => user.name === name)){
-            // console.log(`User ${name} finded`);
-            return res.sendStatus(409);
-        }else{
-            db.collection("participants").insertOne({
+    // Carrega a lista de participantes
+    try{
+        const participants = await db.collection("participants").find().toArray()
+        if(participants){
+            participantsList = participants;
+        }
+    }catch(error){
+        return res.status(500).send(error.message);
+    }
+    // Verifica se o nome ja está na lista de participantes
+    if(participantsList.find((user) => user.name === name)){
+        // console.log(`User ${name} finded`);
+        return res.sendStatus(409);
+    }
+
+    // Faz a validação do nome de usuário
+    if(nameSchema.validate({username: name}).error !== undefined){
+        return sendStatus(422);
+    }else{
+
+        try{
+            await db.collection("participants").insertOne({
                 name: name,
                 lastStatus: Date.now()
             })
-            .then(() => {
-                db.collection("messages").insertOne({
-                    from: name, 
-                    to: 'Todos', 
-                    text: 'entra na sala...', 
-                    type: 'status', 
-                    time: time
-                })
-                res.sendStatus(201)
+            await db.collection("messages").insertOne({
+                from: name, 
+                to: 'Todos', 
+                text: 'entra na sala...', 
+                type: 'status', 
+                time: time
             })
-            .catch(() => {
-                console.log("Algum erro ocorreu ESTOU NO ELSE")
-                
-            });
+            return res.sendStatus(201)
+            
+        }catch(error){  
+            console.log("Algum erro ocorreu ESTOU NO ELSE")
+            return res.sendStatus(422);
         }
-    }else{
-        res.sendStatus(422);
     }
 
 })
@@ -110,23 +122,23 @@ app.post("/participants", (req, res) => {
 
 // GET Messages ---------------------------------------------------
 
-app.get("/messages", (req, res) => {
+app.get("/messages", async(req, res) => {
     const {user} = req.headers;
     const {limit} = req.query;
     
     if((limitSchema.validate({Limit: limit}).error)){
         return res.sendStatus(422);
     }
-    else{
-        db.collection("messages").find( { $or: [ {to: "Todos"}, { to: user }, { from: user } ] } ).toArray()
-        .then((messages) => {
-            if(limit){
-                return res.status(200).send(messages.slice(-limit));
-            }else{
-                return res.status(200).send(messages);
-            }
-        })
-        .catch(() => {return res.sendStatus(422)})
+    
+    try{
+        const messages = await db.collection("messages").find( { $or: [ {to: "Todos"}, { to: user }, { from: user } ] } ).toArray()
+        if(limit){
+            return res.status(200).send(messages.slice(-limit));
+        }else{
+            return res.status(200).send(messages);
+        }
+    }catch(error){
+        return res.sendStatus(422)
     }
 })
 
@@ -135,13 +147,19 @@ app.get("/messages", (req, res) => {
 
 // POST Messages --------------------------------------------------
 
-app.post("/messages", (req, res) => {
+app.post("/messages", async (req, res) => {
     const { to, text, type } = req.body;
     const { user } = req.headers;
     const time = dayjs().format("H:mm:ss");
     
-    if(messageSchema.validate({To: to, Text: text, Type: type}).error === undefined){
-        if(participantsList.find((participant) => participant.name === user)){
+    if(messageSchema.validate({To: to, Text: text, Type: type}).error !== undefined){
+        return res.sendStatus(422);
+    }
+        if(!participantsList.find((participant) => participant.name === user)){
+            return res.sendStatus(422);
+        }
+
+        try{
             db.collection("messages").insertOne({
                 from: user, 
                 to: to, 
@@ -149,15 +167,12 @@ app.post("/messages", (req, res) => {
                 type: type, 
                 time: time
             })
-            .then(() => {return res.sendStatus(201)})
-            .catch(() => {return res.sendStatus(422)});
-        }else{
-            // console.log(messageSchema.validate({To: to, Text: text, Type: type}.error));
-            return res.sendStatus(422);
+            return res.sendStatus(201)
+        }catch(error){
+            console.log(messageSchema.validate({To: to, Text: text, Type: type}).error)
+            return res.sendStatus(422)
         }
-    }else{
-        return res.sendStatus(422);
-    }
+        
 
 })
 
@@ -166,25 +181,51 @@ app.post("/messages", (req, res) => {
 
 // POST Status
 
-app.post("/status", (req, res) => {
+app.post("/status", async (req, res) => {
     const { user } = req.headers;
-    // console.log(user);
-    if(user){
-        if(participantsList.find((participant) => participant.name === user)){
-            db.collection("participants").updateOne({name: user},{$set:{lastStatus:Date.now()}})
-            // WriteResult({ "nMatched" : 1, "nUpserted" : 0, "nModified" : 1 })
-                .then(() => res.status(200).send("Deu certo aqui oh"))
-                .catch()
-        }else{
-            res.sendStatus(404);
-        }
-    }else{
-        res.sendStatus(404)
+    
+    if(!user){
+        return res.sendStatus(404);
+    }
+
+    if(!participantsList.find((participant) => participant.name === user)){
+        return res.sendStatus(404);   
+    }
+
+    try{
+        await db.collection("participants").updateOne({name: user},{$set:{lastStatus:Date.now()}})
+        return res.sendStatus(200);
+    }catch(error){
+        return res.sendStatus(404);
     }
 })
 
 // Fim POST Status
 
+
+// Remoção automática AFK
+
+// setInterval(() => {
+//     const now = Date.now() - 10000;
+//     app.get("/participants", (req, res) => {
+//         db.collection("participants").find( { lastStatus: { $lt: now} } ).toArray()
+//         .then((participantsAFK) => {
+//             console.log(participantsAFK);
+//             db.collection("teste").insertMany(participantsAFK)
+//                 .then(() => {
+//                     console.log("Adicionou oh");
+//                     res.sendStatus(201); 
+//                 })
+//                 .catch(() => {
+//                     console.log("vish, deu ruim");
+//                     res.sendStatus(402); 
+//                 })
+//         })
+//         .catch(() => res.sendStatus(404))
+//     })
+// }, 15000)
+
+// Fim Remoção automática AFK
 
 
 
